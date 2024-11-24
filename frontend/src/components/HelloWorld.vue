ff2a15ad3d99050cbc440e3e76b6ec3743f1f54a
timmyTurner013 <riad.rahman.013@gmail.com>
2024-11-24 20:13:02 +0600

    Initial Commit with backend and frontend setup

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const message = ref("sushi-ya");
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:3000");
+    message.value = response.data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
